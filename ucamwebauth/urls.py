11e88984c6d4fb19b7e21805901ae4198a6957a1
Matias Silva <git@matiasilva.com>
2022-10-10 22:55:33 +0100

    Update endpoints

--- ucamwebauth/urls.py
+++ ucamwebauth/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from ucamwebauth.views import raven_login, raven_logout, raven_return
 
 urlpatterns = [
-    path('accounts/login/', raven_login, name='raven_login'),
-    path('accounts/logout/', raven_logout, name='raven_logout'),
+    path('accounts/login/raven', raven_login, name='raven_login'),
+    path('accounts/logout/raven', raven_logout, name='raven_logout'),
     path('raven_return/', raven_return, name='raven_return'),
 ]
